abdde41e952bf83dc1f515d827d115d5eca118a7
Lucio <lucio.di.capua@gmail.com>
2020-02-17 16:03:58 +0100

    document upgraded with more documents classes

--- exerc/mongo/ex3.py
+++ exerc/mongo/ex3.py
@@ -1,20 +1,19 @@
+from mongo.My_Documents.scheda_ospite import *
 from mongoengine import *
-from pymongo import *
-from mongo.scheda_ospite import *
 from PyQt5.QtCore import QDate
 
 connect('test_db', host='localhost', port=27017)
 
-ospite = SchedaOspite(
-    nome='Lucio',
-    cognome='Di Capua',
-    telefono='+393343519984',
-    email='lucio.di.capua@gmail.com',
-    arrivo=QDate(2020,2,25),
-    partenza=QDate(2020,3,3),
-    totale_ospiti=4,
-    totale_bambini=1,
-).save()
+# ospite = SchedaOspite(
+#     nome='Lucio',
+#     cognome='Di Capua',
+#     telefono='+393343519984',
+#     email='lucio.di.capua@gmail.com',
+#     arrivo=QDate(2020,2,25),
+#     partenza=QDate(2020,3,3),
+#     totale_ospiti=4,
+#     totale_bambini=1,
+# ).save()
 
 for obj in SchedaOspite.objects():
     print('#'*10)
